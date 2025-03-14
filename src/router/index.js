beea7cd49f7f7bd3be1f093582f4dbb16725a5c5
Joaquin Chavez <157412200+WackoFlaka@users.noreply.github.com>
2025-03-14 15:39:11 -0600

    Fixed router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Parties from '../views/Parties.vue';
 import About from '../views/About.vue';
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // ✅ Must use hash mode for GitHub Pages
   routes
 });
 
